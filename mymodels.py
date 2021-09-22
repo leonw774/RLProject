3f7189814e596036699c3548afd49e111a3cee5e
leonw774 <33317554+leonw774@users.noreply.github.com>
2021-09-23 00:42:20 +0800

    change name & corrected model training

--- mymodels.py
+++ mymodels.py
@@ -19,7 +19,7 @@ class ActorCritic:
 
         self.initModels()
         self.critic.compile(loss = "mse", optimizer = self.c_optimizer)
-        self.qNet.compile(loss = "mse", optimizer = self.q_optimizer)
+        self.valueNet.compile(loss = "mse", optimizer = self.q_optimizer)
         
         if load_weight_name :
             self.actor.load_weights("actor_" + load_weight_name)
@@ -29,7 +29,7 @@ class ActorCritic:
         input_scrshots = Input(self.scrshot_size) # screen shot image
         input_action_parameters = Input((4,))
 
-        ### Make Actor
+        ### Make Actor (P(s))
         a = Conv2D(8, (4, 4), padding = "valid", activation = "relu")(input_scrshots)
         a = MaxPooling2D((3, 3), padding = "same")(a)
         a = Conv2D(16, (4, 4), padding = "valid", activation = "relu")(a)
@@ -44,7 +44,7 @@ class ActorCritic:
         self.actor = Model(input_scrshots, conc_action)
         self.actor.summary()
 
-        ### Make Critic
+        ### Make Critic (Q(s, a))
         x = Conv2D(8, (4, 4), padding = "valid", activation = "relu")(input_scrshots)
         x = MaxPooling2D((3, 3), padding = "same")(x)
         x = Conv2D(16, (4, 4), padding = "valid", activation = "relu")(x)
@@ -56,12 +56,11 @@ class ActorCritic:
         self.critic = Model([input_scrshots, input_action_parameters], score)
         self.critic.summary()
         
-        self.critic.trainable = False
         input_scrshots = Input(self.scrshot_size)
         action = self.actor(input_scrshots)
         pred_q = self.critic([input_scrshots, action])
-        self.qNet = Model(input_scrshots, pred_q)
-        self.qNet.summary()
+        self.valueNet = Model(input_scrshots, pred_q)
+        self.valueNet.summary()
     
     def decision(self, cur_shot, temperature = 1.0):
         return Action(np.squeeze(self.actor.predict(cur_shot)))
@@ -70,13 +69,12 @@ class ActorCritic:
         # make next predicted reward array and train input array at same time
         new_q = np.zeros(cfg.train_size)
         for j in range(cfg.train_size):
-            pred_new_q = self.qNet.predict(np.expand_dims(trn_s[j+1], axis=0))
+            pred_new_q = self.valueNet.predict(np.expand_dims(trn_s[j+1], axis=0))
             new_q[j] = trn_r[j] + cfg.gamma * pred_new_q
         trn_s = trn_s[:cfg.train_size]
         trn_a = trn_a[:cfg.train_size]
-        closs = self.critic.train_on_batch([trn_s, trn_a], new_q)
-        qloss = self.qNet.train_on_batch(trn_s, new_q)
-        return (qloss, closs)
+        loss = self.valueNet.train_on_batch(trn_s, new_q)
+        return loss
         
     def save(self, save_weight_name):
         self.actor.save(save_weight_name)
